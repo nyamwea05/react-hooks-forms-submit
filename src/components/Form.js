26e1d71165a7b8db144b571f8ba89122040a8c62
sandra <nyamweasandra@gmail.com>
2023-04-30 21:51:37 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,11 +13,42 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (firstName === "" || lastName === "") {
+      setErrors(["Please enter both first and last name."]);
+    } else {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      console.log(formData);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    }
+  }
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input type="text" value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <br />
+      <label>
+        Last Name:
+        <input type="text" value={lastName} onChange={handleLastNameChange} />
+      </label>
+      <br />
       <button type="submit">Submit</button>
+      {errors.length > 0 && (
+        <ul>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
     </form>
   );
 }
